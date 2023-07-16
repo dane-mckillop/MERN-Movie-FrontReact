1f5e3d77f23ff71fc9fb9b4de41ab732a6541007
Dane <dane.mckillop@gmail.com>
2023-07-16 17:54:08 +1000

    Update description in useInterval.js header

--- src/useInterval.js
+++ src/useInterval.js
@@ -1,7 +1,10 @@
 import { useEffect, useRef } from 'react';
 
 /**
- * Calls a provided function at a specified interval using setInterval() hook.
+ * This custom hook enables calling a provided function at a specified interval using the setInterval() function.
+ * The hook takes in a callback function and an interval duration in milliseconds.
+ * The provided callback function will be executed repeatedly at the specified interval.
+ * This hook uses the useRef() and useEffect() hooks to manage the interval and maintain the latest callback function.
  *
  * @param {function} callback - The function to be called at the specified interval.
  * @param {number} interval - The interval duration in milliseconds.
